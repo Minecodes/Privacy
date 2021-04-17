61957806cb0f32f994e57489bd3c28e4fb54370f
Minecodes <65566012+Minecodes@users.noreply.github.com>
2021-04-17 13:27:37 +0200

    Update supercookie-deleter.ps1

--- supercookie-deleter.ps1
+++ supercookie-deleter.ps1
@@ -1,6 +1,6 @@
 # This is for browser that doesn't supports to delete the f-cache (Favicons (you can use them too for tracking))
 # You haven't to use this script in brave, but you can use it there too.
-# Supported Browsers: Chrome, Brave
+# Supported Browsers: Chrome, Brave, Chromium
 
 #filepaths
 # Google Chrome
@@ -9,6 +9,9 @@ $chrome2 = "$env:LOCALAPPDATA\Google\Chrome\User Data\Default\Favicons-journal"
 #Brave
 $brave1 = "$env:LOCALAPPDATA\BraveSoftware\Brave-Browser\User Data\Default\Favicons"
 $brave2 = "$env:LOCALAPPDATA\BraveSoftware\Brave-Browser\User Data\Default\Favicons-journal"
+#Chromium
+$chromium1 = "$env:LOCALAPPDATA\Chromium\User Data\Default\Favicons"
+$chromium2 = "$env:LOCALAPPDATA\Chromium\User Data\Default\Favicons-journal"
 
 #deleting the files
 # Google Chrome
@@ -32,4 +35,15 @@ if (Test-Path $brave2 -PathType leaf)
 {
     #delete the favicon files
     rm $brave2
+}
+# Chromium
+if (Test-Path $chromium1 -PathType leaf)
+{
+    #delete the favicon files
+    rm $chromium1
+}
+if (Test-Path $chromium2 -PathType leaf)
+{
+    #delete the favicon files
+    rm $chromium2
 }
